3fe4bc98db66577e44037afe4baa151d3871a799
Ibrahima Diallo <iboudiallo84@gmail.com>
2020-10-15 17:48:40 +0200

    add abstract class user (#3)
    * add abstract class user
    
    * test functionnel/unitfor registration
    
    Co-authored-by: Ibrahima D <ibrahima.diallo@display-interactive.com>

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+namespace App\Controller;
+
+use App\Entity\Customer;
+use App\Entity\Producer;
+use App\Form\RegistrationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * Class SecurityController
+ * @package App\Controller
+ */
+class SecurityController extends AbstractController
+{
+
+    /**
+     * @param string $role
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     * @return Response
+     * @Route("/registration/{role}", name="registration")
+     */
+    public function registration(
+        string $role,
+        Request $request,
+        UserPasswordEncoderInterface $userPasswordEncoder
+    ): Response {
+
+        $user = Producer::ROLE === $role ? new Producer : new Customer;
+        $form = $this->createForm(RegistrationType::class, $user)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $userPasswordEncoder->encodePassword($user, $user->getPlainPassword())
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre inscription est validée avec succès !');
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render(
+            'ui/security/registration.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+}
---
+++ tests/RegistrationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * Class RegistrationTest
+ * @package App\Tests
+ */
+class RegistrationTest extends WebTestCase
+{
+
+    /**
+     * @param string $role
+     * @dataProvider provideRoles
+     */
+    public function testSuccessfulRegistration(string $role): void
+    {
+        $client = static::createClient();
+
+        /**
+         * @var RouterInterface $router
+         */
+        $router = $client->getContainer()->get('router');
+
+        $crawler = $client->request(Request::METHOD_GET, $router->generate('registration', [
+            'role' => $role
+        ]));
+
+        $form = $crawler->filter("form[name=registration]")
+            ->form([
+                "registration[email]" => "email@email.com",
+                "registration[plainPassword]" => "dev123",
+                "registration[firstName]" => "firstName tester John",
+                "registration[lastName]" => "lastName tester W.",
+                "registration[email]" => "email@email.com",
+            ]);
+
+        $client->submit($form);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+    }
+
+    public function provideRoles(): Generator
+    {
+        yield ['producer'];
+        yield ['costumer'];
+    }
+}
